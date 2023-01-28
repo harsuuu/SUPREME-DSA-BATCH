e6849f8a9ef575c383b9d872d96f9dcc4e53b597
harsuuu <harshjoshi7895@gmail.com>
2023-01-28 14:00:18 +0530

    Basics of C++

---
+++ FISRT PROGRAM/first.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Namaste Bharat"<<endl;
+    return 0;
+}
+
---
+++ FISRT PROGRAM/input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+int number;
+cout<<"Enter a number :";
+cin>>number;
+cout<<"Entered Number is :"<<number<<endl;
+return 0;
+}
