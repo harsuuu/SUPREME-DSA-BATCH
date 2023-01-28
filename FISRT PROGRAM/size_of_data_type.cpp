f278d5d0461e737bd3fc7b29874989f2e00ef304
harsuuu <harshjoshi7895@gmail.com>
2023-01-28 14:01:32 +0530

    Basic of C++

---
+++ FISRT PROGRAM/size_of_data_type.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a = 1;
+float b = 1.25;
+char c ='d';
+long long d = 245;
+bool e = 5;
+
+cout<<sizeof(a)<<endl;
+cout<<sizeof(b)<<endl;
+cout<<sizeof(c)<<endl;
+cout<<sizeof(d)<<endl;
+cout<<sizeof(e)<<endl;
+return 0;
+}
